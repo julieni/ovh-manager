36a2b74d56d71a3363748b1d42e2d9ccb63f829d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2019-10-31 15:12:32 +0100

    feat(veeam-enterprise): migrate veeam-enterprise
    `veeam-enterprise` is now a standalone module.
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/veeam-enterprise/src/index.js
+++ packages/manager/apps/veeam-enterprise/src/index.js
@@ -1,9 +1,20 @@
-import 'script-loader!jquery'; // eslint-disable-line
-import '@ovh-ux/manager-veeam-enterprise';
+/* eslint-disable import/no-webpack-loader-syntax */
+import 'script-loader!jquery';
+import 'script-loader!moment/min/moment-with-locales.min';
+/* eslint-enable import/no-webpack-loader-syntax */
 
 import angular from 'angular';
 
+import '@ovh-ux/manager-core';
+import '@ovh-ux/manager-veeam-enterprise';
+
+import { momentConfiguration } from './config';
+
+import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
+
 angular
-  .module('veeam-enterpriseApp', [
+  .module('veeamEnterpriseApp', [
+    'ovhManagerCore',
     'ovhManagerVeeamEnterprise',
-  ]);
+  ])
+  .config(momentConfiguration);
